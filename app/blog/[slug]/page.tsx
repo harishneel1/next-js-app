b28e800dc27fee5caa9895a73353afe5a16b876c
Mohd Arish <arishmd768@gmail>
2024-09-17 01:19:21 +0530

    change in code

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,5 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}</h1>;
+  return <h1>Slug@@@@@: {params.slug}</h1>;
 }
