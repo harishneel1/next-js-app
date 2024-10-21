5a2328117cdf736a284e5ee909961fa3521b5337
KamalRepos <kamal.moorthy1@gmail.com>
2024-10-21 15:23:24 -0400

    updated the test to pass

--- app/counter.test.tsx
+++ app/counter.test.tsx
@@ -6,7 +6,7 @@ import Counter from "./counter";
 
 it("App Router: Works with Client Components (React State)", () => {
   render(<Counter />);
-  expect(screen.getByRole("heading")).toHaveTextContent("1");
+  expect(screen.getByRole("heading")).toHaveTextContent("0");
   fireEvent.click(screen.getByRole("button"));
   expect(screen.getByRole("heading")).toHaveTextContent("1");
 });
